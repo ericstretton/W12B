0d80da9d27dfdc769dc2a456dd9610c5a2fc48af
ericstretton <eric.stretton@gmail.com>
2022-04-08 23:46:41 -0600

    Commit before assignment submission.

--- js/app.js
+++ js/app.js
@@ -12,3 +12,23 @@ function handleFunction() {
         activity.innerText = data.activity;
       });
   }
+
+
+  function handleGroupFunction() {
+    getGroupData();
+  }
+
+  const selectPart = document.querySelector('#participantSelect');
+
+  function getGroupData() {
+    var groupDescription = document.getElementById("groupActivityDescription");
+    groupDescription.style.display = "block";
+    
+    fetch("http://www.boredapi.com/api/activity?participants=2")
+      .then(response => response.json())
+      .then(data => {
+        var activity = document.getElementById("groupActivity");
+        activity.innerText = data.activity;
+      });
+  }
+
